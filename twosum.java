f40ed5cd22d028ee1a48399bfca3e289964dc04a
Bhuvana A M <bhuvanaam76@gmail.com>
2025-10-20 19:11:30 +0530

    Implement two-sum function in twosum.java

---
+++ twosum.java
@@ -0,0 +1,15 @@
+public class Main{
+   public int[ ] twosum(int [ ] nums,int target){
+      int[ ] arr=new int[2];
+      for(int I=0;i<nums.length;i++){
+          for(int j=i+1;j<nums.length;j++){
+              if(nums[i]+nums[j]==target){
+                 arr[0]=i;
+                 arr[1]=j;
+                 break;
+              }
+           }
+      }
+      return arr;
+   }
+}
